1a3503a2209468ba8041164f23a940802622f3e3
David A Pears <davidapears@gmail.com>
2018-08-29 08:16:16 +0100

    CSS rejigged so that 'play music' button/link is more prominent

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -6,10 +6,11 @@ const SongDetail = function (props) {
     <div className="results-box">
       <p>#{props.chartPosition +1}</p>
       <p>"{props.song}"</p>
-      <p>Artist: {props.artist}</p>
-      <p> {props.artist} </p>
-      <img src={props.image}/>
       <a href={props.track} target="_blank">*PLAY SONG HERE</a>
+      <p>Artist: {props.artist}</p>
+      {/* <p> {props.artist} </p> */}
+      <img src={props.image}/>
+      <br></br>
     </div>
   )
 }
